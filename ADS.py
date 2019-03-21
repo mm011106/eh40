2520b4952083c22f1c2ffcce462e5870cf1213d3
mm011106 <mm011106@gmail.com>
2019-03-21 20:23:02 +0900

    add muti read function

--- ADS.py
+++ ADS.py
@@ -18,6 +18,7 @@ _CONFIG_MUX={
     '2G': 0x6000,
     '3G': 0x7000
 }
+_MASK_MUX=0x7000
 
 _CONFIG_RANGE = {
     '6V':    0x0000,
@@ -27,6 +28,7 @@ _CONFIG_RANGE = {
     '0.5V':  0x0A00,
     '0.25V': 0x0C00
 }
+_MASK_RANGE=0x0E00
 
 _CONFIG_RATE = {
     '8SPS':    0x0000,
@@ -38,6 +40,7 @@ _CONFIG_RATE = {
     '475SPS':  0x00C0,
     '860SPS':  0x00E0
 }
+_MASK_RATE=0x00E0
 
 _CONFIG_CONV_CONTINUOUS = 0x0000
 _CONFIG_CONV_SINGLE = 0x0100
@@ -81,6 +84,28 @@ def readout(bus, address):
 		adc -= 65535
 	return adc
 
+def readout_all_SE(bus, address):
+	ADC_config = \
+	_CONFIG_OS['START'] | _CONFIG_MUX['0G'] | _CONFIG_CONV_MODE['SINGLE'] \
+	| _CONFIG_RANGE['2V'] | _CONFIG_RATE['128SPS'] \
+	| _CONFIG_COMP_QUE_DISABLE | _CONFIG_COMP_RANGE['NORM']
+
+	print format(ADC_config, "04x")
+
+	ADC_config = ADC_config & (~ _MASK_MUX)) | _CONFIG_MUX['1G']
+	print format(ADC_config, "04x")
+
+	ADC_config = ADC_config & (~ _MASK_MUX)) | _CONFIG_MUX['2G']
+	print format(ADC_config, "04x")
+
+	ADC_config = ADC_config & (~ _MASK_MUX)) | _CONFIG_MUX['3G']
+	print format(ADC_config, "04x")
+
+
+	return 0
+
+def readout_all_DIFF(bus, address):
+	return 0
 
 if __name__ == '__main__':
 
