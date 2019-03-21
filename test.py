577cc1a9c0ae1af98888b05e2805eeebaa146327
mm011106 <mm011106@gmail.com>
2019-03-21 19:28:48 +0900

    add ADC read function

--- test.py
+++ test.py
@@ -12,7 +12,15 @@ bus = SMBus(bus_number)
 if __name__ == '__main__':
 
 	bme280.setup(bus,bme280_address)
-	print _CONFIG_OS['START']
+
+	ADC_config = \
+	ADS._CONFIG_OS['START'] | ADS._CONFIG_MUX['0G'] | ADS._CONFIG_CONV_MODE['SINGLE'] \
+	| ADS._CONFIG_RANGE['2V'] | ADS._CONFIG_RATE['128SPS'] \
+	| ADS._CONFIG_COMP_QUE_DISABLE | ADS._CONFIG_COMP_RANGE['NORM']
+
+	ADS.setCondition(bus, ADS_address, ADC_config)
+
+	print ADS.readout(bus,ADS_address)
 
 	temperature, pressure, humidity = bme280.readData(bus, bme280_address)
 #	print bme280.digT
