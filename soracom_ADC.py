9d436e1310c68473587083c7b4b80ba8ce809059
mm011106 <mm011106@gmail.com>
2019-03-30 17:27:52 +0900

    add comments

--- soracom_ADC.py
+++ soracom_ADC.py
@@ -15,13 +15,14 @@ from contextlib import closing
 
 from smbus import SMBus
 import ADS
+# import bme280
 
 import os
 import commands
 import time
+import logging
 
 # logger setup
-import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
@@ -49,32 +50,39 @@ def soraSend(hostName,portNumber,payload):
     return ret
     #print(soracom.recv(1024))
 
-
+#  constants for connecting to the service
 hostName='harvest.soracom.io'
 portNumber=8514
 resultSend=''
 
+# constants for I2C device
 bus_number  = 1
 bme280_address = 0x76
 ADS_address = 0x48
 
+# make I2C bus instance
 bus = SMBus(bus_number)
 
 if __name__ == '__main__':
 
     ADS.init(bus, ADS_address)
 
+    # bme280.setup(bus,bme280_address)
+
+# ADC1115 configuration
     ADC_config = ADS._CONFIG_DEFAULT & (~ ADS._MASK_RATE) | ADS._CONFIG_RATE['8SPS']
     ADC_config = ADC_config & (~ ADS._MASK_RANGE) | ADS._CONFIG_RANGE['4V']
+	dummy = ADS.setCondition(bus, ADS_address, ADC_config)
     print  '> {0:x}'.format(ADC_config)
 
+# mesurement cycle in sec
     interval = 18
 
     while True:
 #
         data = ADS.readoutMulti(bus, ADS_address, ['01','23'])
         data = [data[0]/32767.*4.096*4., data[1]/32767.*4.096*2.]
-
+        # temperature, pressure, humidity = bme280.readData(bus, bme280_address)
         # payload = '\"temp\":{0[0]:.3f} ,\"humid\":{0[2]:.3f} ,\"atmPressure\":{0[1]:.2f}'.format(data)
         payload = '\"level\":{0[0]:2.5f} ,\"pressure\":{0[1]:2.5f} '.format(data)
         payload = "{" + payload + "}"
