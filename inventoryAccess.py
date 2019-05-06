c461659d047e3f29999302d442f99f0aa4b42694
Masakazu Miyamoto <mm011106@users.noreply.github.com>
2019-05-06 17:18:00 +0900

    create script for Harvest via internet

---
+++ inventoryAccess.py
@@ -0,0 +1,24 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+
+
+import json
+import requests
+
+# id & secure key for access
+idFile='./id.json'
+
+f=open(idFile, 'r')
+id=json.load(f)
+f.close()
+
+headers = {
+    'x-device-secret': id['secretKey'] ,
+    'Content-Type': 'application/json'
+}
+
+url = 'https://api.soracom.io/v1/devices/' + id['deviceId'] + '/publish'
+
+data = {'temp':36}
+
+response = requests.post(url , headers=headers, data=json.dumps(data))
